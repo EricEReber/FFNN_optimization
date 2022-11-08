ff764d7a53399349511013d5704871b8e1a236e2
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-08 12:09:23 +0100

    Return scores dict

--- src/task_a.py
+++ src/task_a.py
@@ -65,7 +65,7 @@ optimal_lambdas = np.zeros(len(schedulers))
 optimal_batches = np.zeros(len(schedulers), dtype=int)
 minimal_errors = np.zeros(len(schedulers))
 
-neural = FFNN(dims)
+neural = FFNN(dims, seed=1337)
 for i in range(len(schedulers)):
     plt.subplot(321 + i)
     plt.suptitle("Test loss for eta, lambda grid", fontsize=22)
@@ -87,7 +87,7 @@ for i in range(len(schedulers)):
         lam,
         batch_sizes,
         params_list[i],
-        batches=X.shape[0]//8,
+        batches=X.shape[0] // 8,
         epochs=epochs // 2,
     )
 
@@ -120,21 +120,22 @@ for i in range(len(schedulers)):
         f"{schedulers[i].__name__} \n {optimal_eta[i]=}\n {optimal_lambdas[i]=} \n {optimal_batches[i]=}\n{minimal_errors[i]=}"
     )
     # neural.read(f"comparison{i}")
-    neural = FFNN(dims)
-    _, test_error, _, _ = neural.fit(
+    neural = FFNN(dims, seed=1337)
+    scores = neural.fit(
         # X_train[:, 1:3],
         X_train,
         z_train,
         schedulers[i],
         *optimal_params_list[i],
         # batches=optimal_batches[i],
-        batches=X.shape[0]//8,
+        batches=X.shape[0] // 8,
         epochs=epochs,
         lam=optimal_lambdas[i],
         # X_test=X_test[:, 1:3],
         X_test=X_test,
         t_test=z_test,
     )
+    test_error = scores["test_error"]
     plt.plot(test_error, label=f"{schedulers[i].__name__}")
     plt.legend(loc=(1.04, 0))
 best_MSE_analytically = np.zeros(epochs)
