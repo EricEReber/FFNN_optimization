900e102bb4d9f4deca68fc3c9c6055d0e27254b5
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-11-11 16:17:22 +0100

    More on task d

--- src/task_d.py
+++ src/task_d.py
@@ -17,19 +17,16 @@ cancer = load_breast_cancer()
 
 X = cancer.data
 z = cancer.target
-
-X_train, X_test, z_train, z_test = train_test_split(X, z)
-
-z_train = z_train.reshape(z_train.shape[0], 1)
-z_test = z_test.reshape(z_test.shape[0], 1)
+z = z.reshape(z.shape[0], 1)
 
 # epochs to run for
 epochs = 200
 folds = 5
 
+dims = (30, 60, 1)
+
 # no hidden layers, no activation function
-dims = (X.shape[1], 1)
-neural = FFNN(dims, seed=1337, hidden_func=)
+neural = FFNN(dims, hidden_func=RELU, output_func=sigmoid, cost_func=CostLogReg)
 
 # parameters to test for
 eta = np.logspace(-5, -1, 5)
@@ -40,29 +37,34 @@ rho = 0.9
 rho2 = 0.999
 
 # batches to test for
-batches_list = np.logspace(
-    0, np.log(X_train.shape[0] + 1), 7, base=np.exp(1), dtype=int
-)
+batches_list = np.logspace(0, np.log(X.shape[0] + 1), 7, base=np.exp(1), dtype=int)
 
 # schedulers to test for
 sched = Adam
 adam_params = [rho, rho2]
+adam_params = [0.005, rho, rho2]
 
-optimal_params, optimal_lambda, loss_heatmap = neural.optimize_scheduler(
-    X_train,
-    z_train,
-    X_test,
-    z_test,
-    sched,
-    eta,
-    lam,
-    adam_params,
+# optimal_params, optimal_lambda, loss_heatmap = neural.optimize_scheduler(
+#     X,
+#     z,
+#     sched,
+#     eta,
+#     lam,
+#     adam_params,
+#     batches=7,
+#     epochs=epochs // 2,
+#     folds=folds,
+# )
+
+neural.cross_val(
+    5,
+    X,
+    z,
+    Adam,
+    *adam_params,
     batches=7,
-    epochs=epochs // 2,
-    folds=folds,
+    epochs=1000,
 )
 
 print(optimal_params)
 print(optimal_lambda)
-
-
